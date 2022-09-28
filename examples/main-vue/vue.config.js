592adf86b2a6d50b19f8427482f4eb5d7be61584
yuhan-yh <853860830@qq.com>
2022-09-28 17:34:19 +0800

    feat: 打包相关修改

--- examples/main-vue/vue.config.js
+++ examples/main-vue/vue.config.js
@@ -1,10 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2022-09-28 14:46:01
+ * @LastEditTime: 2022-09-28 17:32:33
+ * @LastEditors: yuhan
+ * @Description:
+ * @FilePath: \wujie-admin\examples\main-vue\vue.config.js
+ */
 // vue.config.js
 
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/demo-main-vue/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/demo-main-vue/" : "/",
   devServer: {
     headers: {
       "Access-Control-Allow-Origin": "*",
