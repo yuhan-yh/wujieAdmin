05a4d36349981262292fef3dee838ed66bd9c7f2
yuhan-yh <853860830@qq.com>
2022-09-28 23:54:27 +0800

    feat: all页面切换别的页面而不是引用本地项目

--- examples/main-vue/src/fetch.js
+++ examples/main-vue/src/fetch.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-09-28 20:37:14
+ * @LastEditTime: 2022-09-28 22:48:58
+ * @LastEditors: yuhan
+ * @Description:
+ * @FilePath: \wujie-admin\examples\main-vue\src\fetch.js
+ */
 // 携带登录态credentials必须为include
 export default function fetch(url, options) {
   return window.fetch(url, { ...options, credentials: "omit" });
--- examples/main-vue/src/views/Multiple.vue
+++ examples/main-vue/src/views/Multiple.vue
@@ -1,11 +1,16 @@
 <template>
   <div style="height: 100%; width: 100%">
-    <WujieVue class="item" name="react16" :url="react16Url" :sync="true"></WujieVue>
-    <WujieVue class="item" name="react17" :url="react17Url" :sync="true"></WujieVue>
+    <!-- <WujieVue class="item" name="test" :url="test" :sync="true"></WujieVue> -->
+    <WujieVue class="item" name="test1" :url="test1" :sync="true"></WujieVue>
+    <!-- <WujieVue class="item" name="test2" :url="test1" :sync="true"></WujieVue> -->
+    <!-- <WujieVue class="item" name="test3" :url="test1" :sync="true"></WujieVue> -->
+    <!-- <iframe width="400" height="400" src="https://cn.vuejs.org/" frameborder="0"></iframe> -->
+    <!-- <WujieVue class="item" name="react16" :url="react16Url" :sync="true"></WujieVue> -->
+    <!-- <WujieVue class="item" name="react17" :url="react17Url" :sync="true"></WujieVue>
     <WujieVue class="item" name="vue2" :url="vue2Url" :sync="true"></WujieVue>
     <WujieVue class="item" name="vue3" :url="vue3Url" :sync="true"></WujieVue>
     <WujieVue class="item" name="vite" :url="vite" :sync="true"></WujieVue>
-    <WujieVue class="item" name="angular12" :url="angular12Url" :sync="true"></WujieVue>
+    <WujieVue class="item" name="angular12" :url="angular12Url" :sync="true"></WujieVue> -->
   </div>
 </template>
 
@@ -15,12 +20,14 @@ import hostMap from "../hostMap";
 export default {
   data() {
     return {
-      react16Url: hostMap("//localhost:7600/"),
-      react17Url: hostMap("//localhost:7100/"),
-      vue2Url: hostMap("//localhost:7200/"),
-      vue3Url: hostMap("//localhost:7300/"),
-      vite: hostMap("//localhost:7500/"),
-      angular12Url: hostMap("//localhost:7400/"),
+      // test: hostMap("https://wujie-micro.github.io/doc/"),
+      test1: hostMap("https://cn.vuejs.org/"),
+      // react16Url: hostMap("//localhost:7600/"),
+      // react17Url: hostMap("//localhost:7100/"),
+      // vue2Url: hostMap("//localhost:7200/"),
+      // vue3Url: hostMap("//localhost:7300/"),
+      // vite: hostMap("//localhost:7500/"),
+      // angular12Url: hostMap("//localhost:7400/"),
     };
   },
   methods: {
